b103fc34074e07bdad226a4020351e3d1a5e63c7
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-02-19 16:36:33 +0530

    Update set 1-1.cpp

--- set 1-1.cpp
+++ set 1-1.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cout<<"Enter a number\n";
+	cin>>a;
+	if(a>0)
+	{
+		cout<<"Positive";
+	}
+	else if(a<0)
+	{
+		cout<<"Negative";
+	}
+	else
+	{
+		cout<<"Zero";
+	}
+	return 0;
+}
