ca6ba2e835747fc281bdb534c975454f6f9c1b84
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-02-20 16:48:28 +0530

    Create set1-3.cpp

---
+++ set1-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	cout<<"enter the char\n";
+	cin>>ch;
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+		cout<<"the char is vowel";
+		
+	}
+	else{
+		cout<<"the char is consonant";
+	}
+
+	return 0;
+}
