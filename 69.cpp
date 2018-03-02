1b7f08d489dbe2107970c304e45aed3d5b700ac6
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 17:33:42 +0530

    Create 69.cpp

---
+++ 69.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a1,b,n;
+  cout<<"Enter your number";
+  cin>>a1;
+  cout<<"enter your another number";
+  cin>>b;
+  n=a-b;
+  cout<<"the value is"<<n;
+  if(n%2==1)
+  {
+      cout<<"it is odd";
+  }
+  else
+  {
+      cout<<"it is even";
+  }
+  return 0;
+}
