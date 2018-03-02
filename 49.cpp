bd23ebfc2388d2d6dab4ad6b21c71f4f2b10fe5c
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 16:35:08 +0530

    Create 49.cpp

---
+++ 49.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int c,b,sum;
+  cout<<"Enter c number";
+  cin>>a;
+  cout<<"enter b number";
+  cin>>b;
+  sum=c+b;
+  cout<<"the value is"<<sum;
+}
