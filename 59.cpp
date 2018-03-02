ab926c10d4bc4ead124d031679aa839c9c0ce461
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 17:07:08 +0530

    Create 59.cpp

---
+++ 59.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int large, arr[50], size, i;
+	cout<<"Enter array elements : ";
+	for(i=0; i<10;i++)
+	{
+		cin>>arr[i];
+	}
+	cout<<"Searching for largest number ...\n\n";
+	large=arr[0];
+	for(i=0; i<10; i++)
+	{
+		if(large<arr[i])
+		{
+			large=arr[i];
+		}}
+	cout<<"Largest Number = "<<large;
+	return 0;
+}
