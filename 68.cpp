fde9c1e1860ecd7a63e980f34c6bebe3f83db485
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 17:31:54 +0530

    Create 68.cpp

---
+++ 68.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a1,n,i;
+  cout<<"Enter your number";
+  cin>>a1;
+ for(i=0;i<=n;i++)
+{
+    if(a1%7==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+}
+}
