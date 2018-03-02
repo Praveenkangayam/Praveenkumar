ea1cdeb2e4c22c35e3e130d9a99dab7cf21f59e4
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 16:20:59 +0530

    Update 41.cpp

--- 41.cpp
+++ 41.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main()
 {
 int num,i;
-char a[50];
+char b[50];
 cout<<"Enter the string with number";
-cin>>a>>num;
+cin>>b>>num;
 for(i=1;i<=num;i++)
 {
-cout<<a<<"\n";
+cout<<b<<"\n";
 }
 return 0;
 }
