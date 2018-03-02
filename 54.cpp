78b15375362a90f3acbc58560b275be2f2d6537b
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 16:44:02 +0530

    Create 54.cpp

---
+++ 54.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int b;
+  cout<<"Enter your number";
+  cin>>b;
+  if(b%2==0)
+  {
+      b=b;
+      cout<<b;
+  }
+  else
+  {
+      b=b-1;
+      cout<<b;
+  }
+return 0;
+}
