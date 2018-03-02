980869b84ce6a6f7506524eeb8a0582a9efe6f92
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-02 17:09:49 +0530

    Update 59.cpp

--- 59.cpp
+++ 59.cpp
@@ -5,16 +5,16 @@ int main(){
 	cout<<"Enter array elements : ";
 	for(i=0; i<10;i++)
 	{
-		cin>>arr[i];
+	cin>>arr[i];
 	}
 	cout<<"Searching for largest number ...\n\n";
 	large=arr[0];
 	for(i=0; i<10; i++)
 	{
-		if(large<arr[i])
-		{
-			large=arr[i];
-		}}
+	if(large<arr[i])
+	{
+	large=arr[i];
+	}}
 	cout<<"Largest Number = "<<large;
 	return 0;
 }
