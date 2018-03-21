e02cca949c0c594c4c92810a82965d318f5d1fc2
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-03-21 10:32:46 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter the number";
+    cin>>a;
+    if((a>=1)&&(a<=10))
+    {
+       cout<<"yes";
+    }
+   else
+      {
+          cout<<"no";
+      }
+    
+
+    return 0;
+}
