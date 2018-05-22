023b06162130616578eaa7f9ef2fd2dd57ab834c
Praveenkangayam <36624849+Praveenkangayam@users.noreply.github.com>
2018-05-22 10:04:47 +0530

    Update 82.cpp

--- 82.cpp
+++ 82.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 int main() 
 {
-	{
-	float L,B;
-	float area;
-	cout<<"enter the l and b";
-	cin>>L>>B;
-	area=L*B;
-	cout<<area;
+{
+float L,B;
+float area;
+cout<<"enter the l and b";
+cin>>L>>B;
+area=L*B;
+cout<<area;
 }
 return 0;
 }
